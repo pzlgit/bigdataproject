234d6b5a7297bc5e5c09be551f92e6ad2eef4c31
pangzl <1414001474@qq.com>
2022-04-09 18:57:54 +0800

    Scala Project Init

---
+++ Scala/src/main/java/com/atguigu/bigdata/scala/HelloScala.scala
@@ -0,0 +1,10 @@
+package com.atguigu.bigdata.scala
+
+object HelloScala {
+
+  def main(args: Array[String]): Unit = {
+    System.out.print("Hello World")
+    println("Hello Scala")
+  }
+
+}
---
+++ Scala/src/main/java/com/atguigu/bigdata/scala/Scala_comment.scala
@@ -0,0 +1,13 @@
+package com.atguigu.bigdata.scala
+
+object Scala_comment {
+  def main(args: Array[String]): Unit = {
+    // 单行注释
+    /*
+     多行注释
+     */
+    /**
+     * 文档注释
+     */
+  }
+}
