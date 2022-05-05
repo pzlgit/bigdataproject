518cf2f9f5eb7265630ae07f5b1055502522d2b2
pangzl <1414001474@qq.com>
2022-05-05 16:51:48 +0800

    Spark SQL Exec

--- Spark/src/main/java/com/atguigu/bigdata/sparksql/SparkSQL01_input.scala
+++ Spark/src/main/java/com/atguigu/bigdata/sparksql/SparkSQL01_input.scala
@@ -1,24 +1,25 @@
 package com.atguigu.bigdata.sparksql
 
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 /**
  *
  * @author pangzl
- * @create 2022-05-03 18:40
+ * @create 2022-05-05 14:44
  */
 object SparkSQL01_input {
 
   def main(args: Array[String]): Unit = {
-    val spark: SparkSession = SparkSession.builder()
-      .master("local[*]")
-      .appName("SparkSQLTest")
-      .getOrCreate()
-    // 读取数据
+    // 1. 创建上下文环境配置对象
+    val sparkConf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkSQLTest")
+    // 2. 创建SparkSession对象
+    val spark: SparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+    // 3. 读取文件
     val df: DataFrame = spark.read.json("data/user.json")
-    // 展示数据
+    // 4. 可视化展示
     df.show()
-
+    // 5. 关闭资源
     spark.stop()
   }
 }
