b96acd1575d84c26d90dd2d005e65310ecc92805
pangzl <1414001474@qq.com>
2022-07-12 19:49:25 +0800

    Flink 实时项目 DWS层 流量域其它指标统计

--- flink-gmall-realtime/src/main/java/com/atguigu/gmall/realtime/util/ClickHouseUtil.java
+++ flink-gmall-realtime/src/main/java/com/atguigu/gmall/realtime/util/ClickHouseUtil.java
@@ -56,6 +56,8 @@ public class ClickHouseUtil {
                 new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
                         .withDriverName(GmallConfig.CLICKHOUSE_DRIVER)
                         .withUrl(GmallConfig.CLICKHOUSE_URL)
+                        .withUsername("default")
+                        .withPassword("123456")
                         .build()
         );
 
